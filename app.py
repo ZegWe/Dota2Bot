40137b1b19a76df6ee8f743e757c93669c59f0b7
ZegWe <abdccdba147@gmail.com>
2021-01-17 01:12:35 +0800

    fix socketio reconnection bug

--- app.py
+++ app.py
@@ -44,33 +44,16 @@ def init():
 		managers[group].add_plugin('DOTA2战绩播报', True)
 		managers[group].add_plugin('DOTA2每日运势', True)
 
-def heartbeat():
-	while sio_connected:
-		for i in range(50):
-			time.sleep(1)
-			if not sio_connected:
-				break
-		if not sio_connected:
-			break
-		if not sio.connected:
-			continue
-		sio.emit('GetWebConn', Config.bot_id)
-
 if __name__ == "__main__":
 	init()
 	print('Connecting to server...', end='', flush=True)
 	sio.connect(Config.sio_url, transports=['websocket'])
-	sio_connected = True
-	t = threading.Thread(target=heartbeat)
-	# t.start()
 	try:
 		sio.wait()
 	except KeyboardInterrupt:
-		sio_connected = False
 		for group in managers:
 			managers[group].shutdown()
 		PluginDB.disconnect(PluginDB.get_name())
 		DotaDB.disconnect(DotaDB.get_name())
-		# t.join()
 		print('Say you next time~')
 		exit(0)
