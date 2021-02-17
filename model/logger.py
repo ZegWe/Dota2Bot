e82acf08b5f1a0b44624845518e9236369eef09b
ZegWe <abdccdba147@gmail.com>
2021-02-17 11:49:46 +0800

    debug logger improve

--- model/logger.py
+++ model/logger.py
@@ -5,6 +5,8 @@ from loguru import logger
 def init_logger(debug: bool):
 	logger.remove()
 	if debug:
-		logger.add(sys.stdout, level='DEBUG', format='<level>{time:YYYY-MM-DD HH:mm:ss.SSS} {level} {message}</level>')
+		logger.add(sys.stdout, level='DEBUG', format='<level>{time:YYYY-MM-DD HH:mm:ss.SSS} {level} {name}:{function}:{line} - {message}</level>')
 	else:
 		logger.add(sys.stdout, level='INFO', format='<level>{time:YYYY-MM-DD HH:mm:ss.SSS} {level} {message}</level>')
+
+# init_logger(False)
